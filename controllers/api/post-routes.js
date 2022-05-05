6c062df9265ea492de018f54287d2e9f5979cd61
JacobReeder <jacob.reeder1@gmail.com>
2022-05-04 19:55:45 -0700

    post for post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -22,6 +22,7 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+  
 
   router.get('/:id', (req, res) => {
     Post.findOne({
@@ -48,6 +49,45 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  router.post('/', (req, res) => {
+    
+    Post.create({
+      title: req.body.title,
+      post_url: req.body.post_url,
+      user_id: req.body.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+///May not use?
+ /* router.put('/:id', (req, res) => {
+    Post.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });*/
 
   ////May not use?
   /*router.delete('/:id', (req, res) => {
