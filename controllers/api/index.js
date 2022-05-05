a4b99b474dc5f4dafe8ae7acdd2758313da5938d
JacobReeder <jacob.reeder1@gmail.com>
2022-05-04 18:13:41 -0700

    Connected post-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+///Mental note: Leave post-routes.js blank until we merge the feature branch
 
 module.exports = router;
