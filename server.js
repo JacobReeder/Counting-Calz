1e71149b956a6d20a77383a9c3178f90e03b8597
JacobReeder <jacob.reeder1@gmail.com>
2022-05-06 16:29:03 -0700

    Added Handlebars connections and HTML

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); // links the css sheet from 'public'
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
