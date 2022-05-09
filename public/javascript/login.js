0fbfc1b91f57a3a37388a2004b7ced1f203b1fe6
Cole Simpson <cole22simpson@gmail.com>
2022-05-08 19:05:19 -0700

    fixed login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -8,11 +8,12 @@ async function loginFormHandler(event) {
 
   if (email && password) {
     const response = await fetch('/api/users/login', {
-      method: 'POST',
+      method: 'post',
       body: JSON.stringify({
         email,
         password,
       }),
+      headers: { 'Content-Type': 'application/json' }
     });
 
     if (response.ok) {
