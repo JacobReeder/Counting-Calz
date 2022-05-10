e019a6d54d9e309841d4bce9a3444ee9cc2a76bb
Cole Simpson <cole22simpson@gmail.com>
2022-05-10 14:45:43 -0700

    not sure what changes i made here

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,19 +1,32 @@
 const router = require('express').Router();
-const { Post } = require('../models');
+const { Post, Goal } = require('../models');
 
 router.get('/', (req, res) => {
   Post.findAll({
     where: {
       user_id: req.session.user_id,
     },
+    // if no session user id, document.location.replace(/)
   })
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
-
-      res.render('dashboard', {
-        posts,
-        loggedIn: req.session.loggedIn,
-      });
+      Goal.findOne({
+        where: {
+          user_id: req.session.user_id,
+        },
+      })
+        .then((dbPostData2) => {
+          const goal = dbPostData2;
+          res.render('dashboard', {
+            posts,
+            goal,
+            loggedIn: req.session.loggedIn,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
     })
     .catch((err) => {
       console.log(err);
