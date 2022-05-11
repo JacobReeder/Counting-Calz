62cfe8078e8080a6091b1ac52250d5c27e7cede2
Cole Simpson <cole22simpson@gmail.com>
2022-05-11 14:46:04 -0700

    added our meal of the day

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -34,16 +34,30 @@ async function rndMeal() {
   // const url = 'https://www.themealdb.com/api/json/v1/9973533/random.php';
   const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
-  console.log(url);
-
   rndMealEl.innerHtml = '';
   fetch(url)
     .then((res) => res.json())
     .then((res) => {
       console.log(res.meals[0]);
       if (res.meals[0].strSource !== '') {
-        console.log(res.meals[0].strSource);
-        // logic to create meal div stuff
+        const mealLink = res.meals[0].strSource;
+        const mealName = res.meals[0].strMeal;
+        const mealThumbnail = res.meals[0].strMealThumb;
+
+        const linkEl = document.createElement('a');
+        linkEl.setAttribute('href', mealLink);
+        rndMealEl.appendChild(linkEl);
+
+        const thumbnailEl = document.createElement('img');
+        thumbnailEl.classList = ('meal-thumbnail');
+        thumbnailEl.setAttribute('src', mealThumbnail);
+        thumbnailEl.setAttribute('alt', mealName);
+        linkEl.appendChild(thumbnailEl);
+
+        const mealNameEl = document.createElement('h4');
+        mealNameEl.classList = ('meal-name');
+        mealNameEl.textContent = (mealName);
+        rndMealEl.appendChild(mealNameEl);
       } else {
         rndMeal();
       }
