31e5b6dc7cdf6f80888062dd8646e31fa3f77a41
Deck Stream <tneswick2@verizon.net>
2022-05-13 23:53:56 -0700

    next meal button, with logic for outside api moved to mealApi.js

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -1,4 +1,3 @@
-const rndMealEl = document.querySelector('.rnd-meal');
 const goalRevealBtn = document.getElementById('open-goal-btn');
 const goalInputEl = document.getElementById('goal-input');
 const goalSubmitBtn = document.getElementById('goal-submit');
@@ -28,48 +27,6 @@ async function newPostSubmit(event) {
   } else {
     alert(response.statusText);
   }
-}
-
-function rndMeal() {
-  // const url = 'https://www.themealdb.com/api/json/v1/9973533/random.php';
-  const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
-
-  rndMealEl.innerHTML = '';
-  fetch(url)
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res.meals[0]);
-      if (res.meals[0].strSource !== '') {
-        const mealLink = res.meals[0].strSource;
-        const mealName = res.meals[0].strMeal;
-        const mealThumbnail = res.meals[0].strMealThumb;
-
-        const mealTitleEl = document.createElement('h3');
-        mealTitleEl.className = ('fs-3 fw-bold text-white text-center border-bottom border-white border-2 my-4 pb-2');
-        mealTitleEl.textContent = ('Try this meal out!');
-        rndMealEl.appendChild(mealTitleEl);
-
-        const linkEl = document.createElement('a');
-        linkEl.setAttribute('href', mealLink);
-        rndMealEl.appendChild(linkEl);
-
-        const thumbnailEl = document.createElement('img');
-        thumbnailEl.classList = ('meal-thumbnail');
-        thumbnailEl.setAttribute('src', mealThumbnail);
-        thumbnailEl.setAttribute('alt', mealName);
-
-        const mealNameEl = document.createElement('h4');
-        mealNameEl.classList = ('fs-5 text-white text-center my-1');
-        mealNameEl.textContent = (mealName);
-        linkEl.appendChild(thumbnailEl);
-        linkEl.appendChild(mealNameEl);
-      } else {
-        rndMeal();
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 }
 
 function showGoal(event) {
@@ -147,8 +104,6 @@ async function deletePost() {
     alert(delResponse.statusText);
   }
 }
-
-rndMeal();
 
 // event listeners
 document.getElementById('new-post-form').addEventListener('submit', newPostSubmit);
